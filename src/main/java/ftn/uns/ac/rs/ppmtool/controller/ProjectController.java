43141dffd6cd78d96e50277debb0c7fe1fa2aa43
greksadarko <greksadarko@gmail.com>
2021-10-22 16:07:34 +0200

    Delete project by identifier

--- src/main/java/ftn/uns/ac/rs/ppmtool/controller/ProjectController.java
+++ src/main/java/ftn/uns/ac/rs/ppmtool/controller/ProjectController.java
@@ -47,4 +47,11 @@ public class ProjectController {
     public Iterable<Project> getAllProjects() {
         return projectService.findAllProjects();
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProjectByIdentifier(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: '" + projectId + "' was deleted", HttpStatus.OK);
+    }
 }
--- src/main/java/ftn/uns/ac/rs/ppmtool/service/ProjectService.java
+++ src/main/java/ftn/uns/ac/rs/ppmtool/service/ProjectService.java
@@ -34,4 +34,13 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project == null) {
+            throw new ProjectIdException("This project does not exist");
+        }
+        projectRepository.delete(project);
+    }
 }

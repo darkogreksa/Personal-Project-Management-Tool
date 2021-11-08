6b6d3556d68fe6a1364afca83eb0485ce90155bc
greksadarko <greksadarko@gmail.com>
2021-11-08 21:15:40 +0100

    Routing

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard />
+        <Routes>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
